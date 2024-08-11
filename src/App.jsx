8d5a5c7571352612db1e1551f10c27dbdf103fd9
CarlosOyarce <carlosoyarce3@gmail.com>
2024-08-11 17:45:34 -0400

    Hito 2 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-
 import Navbar from './componentes/Navbar'
 import Home from './componentes/Home'
 import RegisterPage from './componentes/RegisterPage'
 import LoginPage from './componentes/LoginPage'
-
-import Home from './componentes/Home'
-import Navbar from './componentes/Navbar'
-
 import Footer from './componentes/Footer'
 import './App.css'
 
